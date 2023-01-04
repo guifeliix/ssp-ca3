7e0afb82a17a475117fdd99d7c10c7fcd89595dd
guifeliix <gui_feliix@yahoo.com>
2023-01-04 20:27:37 +0000

    used the sample script from class and adapted.

--- beermenu.xsl
+++ beermenu.xsl
@@ -1,6 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:transform xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="2.0">
 	<xsl:template match="/">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<link rel="stylesheet" href="/css/style.css"/>
+	</head>
 		<table id="menuTable" border="1" class="indent">
 			<thead>
 				<tr>
@@ -8,6 +12,8 @@
 					<th>Item</th>
 					<th>Price</th>
 				</tr>
+
+				
 			</thead>
 			<tbody>
 				<xsl:for-each select="//category">
--- views/js/scripts.js
+++ views/js/scripts.js
@@ -5,3 +5,68 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+
+var gEntreeCount = 0;
+function calculateBill(idMenuTable) {
+    var fBillTotal = 0.0;
+    var i = 0;
+    var oTable = document.getElementById(idMenuTable);
+    var aCBTags = oTable.getElementsByTagName('INPUT');
+    for (i = 0; i < aCBTags.length; i++) {
+
+        if (aCBTags(i).checked) {
+            var oTR = getParentTag(aCBTags[i], 'TR');
+            var oTDPrice = oTR.getElementsByTagName('TD')[2];
+            fBillTotal += parseFloat(oTDPrice.firstChild.data);
+        };
+
+    };
+    return Math.round(fBillTotal * 100.0) / 100.0;
+
+};
+function highlightZeroAlcohol(idTable, bShowZeroAlcohol) {
+
+    var i = 0;
+    var oTable = document.getElementById(idTable);
+    var oTBODY = oTable.getElementsByTagName('TBODY')[0];
+    var aTRs = oTBODY.getElementsByTagName('TR');
+
+    for (i = 0; i < aTRs.length; i++) {
+
+        if (aTRs[i].getAttribute('zero') && aTRs[i].getAttribute('zero') == "true") {
+
+            if (bShowZeroAlcohol) {
+                aTRs[i].getElementsByClassName.backgroundColor = "lightBlue";
+            } else {
+                aTRs[i].getElementsByClassName.backgroundColor = "";
+            };
+
+        };
+    };
+};
+
+function getParentTag(oNode, sParentType) {
+
+
+    var oParent = oNode.parentNode;
+    while (oParent) {
+
+        if (oParent.nodeName == sParentType)
+            return oParent;
+        oParent = oParent.parentNode;
+
+    };
+    return oParent;
+};
+
+window.addEventListener("load", function () {
+    document.forms[0].txtBillAmt.value = calculateBill('menuTable');
+    document.querySelector("#calcBill").addEventListener("click", function() {
+        document.forms[0].txtBillAmt.value = calculateBill('menuTable');
+
+    });
+    document.querySelector("#showZero").addEventListener("click", function() {
+        highlightZeroAlcohol('menuTable', this.checked);
+    });
+});
