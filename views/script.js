af2543212ef633f7ed5079ac924d0b421969b925
guifeliix <gui_feliix@yahoo.com>
2023-01-06 14:22:26 +0000

    created the script to add and delete rows

--- views/script.js
+++ views/script.js
@@ -37,4 +37,33 @@ function addSelected() {
     var amount = document.getElementById('txtAmount').value;
     var listing = ddloptionbeerlist[index].text;
     var price = ddloptionbeerlist[index].value;
+    let total = price*amount;
+
+    let table = document.getElementById("tdisplay");
+    let row = table.insertRow(-1); 
+
+    let c1 = row.insertCell(0);
+    let c2 = row.insertCell(1);
+    let c3 = row.insertCell(2);
+    let c4 = row.insertCell(3);
+
+    c1.innerText = listing;
+    c2.innerText = price;
+    c3.innerText = amount;  
+    c4.innerText = total;
 }
+
+function deleteRow(ele) {
+    var table = document.getElementById('tdisplay');
+    var rowCount = table.rows.length;
+    if (rowCount <= 1) {
+        alert("There is no row available to delete!");
+        return;
+    }
+    if (ele) {
+        ele.parentNode.parentNode.remove();
+    } else {
+        table.deleteRow(rowCount - 1);
+    }
+}
+
